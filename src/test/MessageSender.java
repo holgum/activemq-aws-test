f5e9ab1d6f46766465ae490b1dcf0382a2b8061a
Manny Holguin <manny.holguin@mac.com>
2022-08-24 12:55:51 -0400

    updated for cbp demo

--- src/test/MessageSender.java
+++ src/test/MessageSender.java
@@ -16,22 +16,25 @@ public class MessageSender {
 	
     //URL of the JMS server. DEFAULT_BROKER_URL will just mean that JMS server is on localhost
     //private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
-//	private static String url = "ssl://b-a7d64cec-e0d9-45b4-9fd5-905dfe0e703d-1.mq.us-east-1.amazonaws.com:61617";
+	private static String url = "ssl://b-d7b6bdc0-0086-4e36-b2b4-5c686259b850-1.mq.us-east-1.amazonaws.com:61617";
 //	private static String url = "tcp://localhost:30612";
 
-	private static String url = "failover:(tcp://localhost:30612,tcp://localhost:30616)";
+	//private static String url = "failover:(tcp://localhost:30612,tcp://localhost:30616)";
 	
 	
     // default broker URL is : tcp://localhost:61616"
 	
-    private static String subject = "TEST.QUEUE"; // Queue Name.You can create any/many queue names as per your requirement. 
+    private static String subject = "ACE.COLLECT.XX"; // Queue Name.You can create any/many queue names as per your requirement. 
      
     public static void main(String[] args) throws JMSException {        
 
 
         // Getting JMS connection from the server and starting it
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection("admin","admin-pw");
+        
+//      Connection connection = connectionFactory.createConnection("holgum","100%uptime!!");
+        Connection connection = connectionFactory.createConnection("appuser","100%uptime!!");
+
         connection.start();
         System.out.println("** starting connection..");
     	
@@ -48,7 +51,6 @@ public class MessageSender {
                     Session.AUTO_ACKNOWLEDGE);  
             System.out.println("** session created..");
  
-            //Destination represents here our queue 'JCG_QUEUE' on the JMS server. 
             //The queue will be created automatically on the server.
             Destination destination = session.createQueue(subject); 
      
@@ -61,7 +63,7 @@ public class MessageSender {
 	        // Here we are sending our message!
 	        producer.send(message);
 	        try {
-				Thread.sleep(20);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
